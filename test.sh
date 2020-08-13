d2522ab8185f53890e040d7396ca923b3a67b868
Gus <43172308+Gaunsessa@users.noreply.github.com>
2020-08-13 17:47:27 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,11 +3,13 @@ input="README.md"
 while IFS= read -r line
 do
     if [ "$line" = "![Test](http://203.214.44.222:8000/cat/)" ]; then
-        # echo "![Test](http://203.214.44.222:8000/cat)" > test.txt
-        curl 203.214.44.222:8000/JOE
+        echo "![Test](http://203.214.44.222:8000/cat)" > README.md
     fi
     if [ "$line" = "![Test](http://203.214.44.222:8000/cat)" ]; then
-        # echo "![Test](http://203.214.44.222:8000/cat/)" > test.txt
-        curl 203.214.44.222:8000/MAMA
+        echo "![Test](http://203.214.44.222:8000/cat/)" > README.md
     fi
+    
+    git config --local user.email "Cat@Bot.Cat"
+    git config --local user.name "CatBot"
+    git commit -m "Add changes" -a
 done < "$input"
