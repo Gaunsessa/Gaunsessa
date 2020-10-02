737429a13f2c40ac7e67f164d388150a0f86d96a
Gus <43172308+Gaunsessa@users.noreply.github.com>
2020-10-02 16:34:55 +1000

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -3,7 +3,10 @@
 files=(cats/*)
 img="${files[RANDOM % ${#files[@]}]}"
 
-echo "![CAT](https://github.com/Gaunsessa/Gaunsessa/blob/master/${img})" > README.md
+echo "
+😳Funny Cat😳
+
+![CAT](https://github.com/Gaunsessa/Gaunsessa/blob/master/${img})" > README.md
 
 git config --local user.email "Cat@Bot.Cat"
 git config --local user.name "CatBot"
