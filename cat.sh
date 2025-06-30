0adc3a70bfee34281bce83dfe9faad3fb2d5a121
Gus <43172308+Gaunsessa@users.noreply.github.com>
2025-06-30 21:36:25 +1000

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -7,6 +7,7 @@ echo "
 Angus - [ef.je](https://ef.je)
 
 Here is a cat:
+
 ![CAT](https://github.com/Gaunsessa/Gaunsessa/blob/master/${img})" > README.md
 
 git config --local user.email "Cat@Bot.Cat"
